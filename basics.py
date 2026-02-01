f769e64d06a56328ecb7626328a7a3455832898c
yashrawat <yashrawat682@gmail.com>
2026-02-01 20:43:23 +0530

    isInstance and Type and immutable and mutable

---
+++ basics.py
@@ -0,0 +1,21 @@
+
+
+print("Variable Names ")
+
+
+name = "Champion"
+
+print(name)
+
+
+# Using type which gives us the Variable type .
+print(type(name)==str)
+
+
+
+print(isinstance(name,str))
+
+
+age =2.9
+
+print(isinstance(age,float))

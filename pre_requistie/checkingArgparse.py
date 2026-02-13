1ef157ca5737f7bf64fa03c90e30bba0504914eb
yashrawat <yashrawat682@gmail.com>
2026-02-13 06:28:10 +0530

    Arg Parser

---
+++ pre_requistie/checkingArgparse.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("--name",default="Raju")
+parser.add_argument("--city",default="Delhi")
+
+args=parser.parse_args()
+
+print(args.name)
+print(args.city)
--- pre_requistie/checkingSys.py
+++ pre_requistie/checkingSys.py
@@ -3,5 +3,6 @@ import sys
 
 if(len(sys.argv)<2):
     print("Please provide the name ")
+    sys.exit()
 
 print("Hello ! "+ sys.argv[1])
